d45bac27be24d0fda8e9ea4f356bcc45a1af822a
Richard Russon (flatcap) <richard.russon@gmail.com>
2015-01-27 22:53:50 +0000

    more route data

--- data.js
+++ data.js
@@ -3,11 +3,11 @@ var m = document.getElementById('message');
 m.innerHTML = "waiting...";
 
 var info = "";
-$.getJSON('data.json', function(data) {
+$.getJSON('routes.json', function(data) {
 	// info = JSON.stringify (data);
 	// info = Object.keys(data);
 	$.each(data.routes, function(index, value) {
-		info += "<h1>" + data.routes[index].directory + "</h1>";
+		info += "<h1>" + index + "</h1>";
 		info += "<ul>";
 		$.each(data.routes[index], function(index, value) {
 			if (value) {
