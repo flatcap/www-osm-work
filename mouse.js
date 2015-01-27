479a4c67839b4d47b9d1cb36c8c4bfd12b3f19bd
Richard Russon (flatcap) <richard.russon@gmail.com>
2015-01-27 03:06:57 +0000

    mouse tracking

--- mouse.js
+++ mouse.js
@@ -11,12 +11,14 @@ layers.push(new ol.layer.Tile({
 	})
 );
 
-layers.push (new ol.layer.Vector({
-	source: new ol.source.KML({
-		projection: proj,
-		url: 'camp.kml',
-		extractStyles: false,
-	}),
+var vectorSource = new ol.source.KML({
+	projection: proj,
+	url: 'mouse.kml',
+	extractStyles: false,
+});
+
+layers.push(new ol.layer.Vector({
+	source: vectorSource,
 	style: new ol.style.Style({
 		image: new ol.style.Circle({
 			radius: 7,
@@ -35,43 +37,57 @@ var map = new ol.Map({
 	target: document.getElementById('map'),
 	layers: layers,
 	view: new ol.View({
-		center: ol.proj.transform([1.315772,51.125442], 'EPSG:4326', 'EPSG:3857'),
-		zoom: 9
+		center: ol.proj.transform([-3.143848, 54.699234], 'EPSG:4326', 'EPSG:3857'),
+		zoom: 6
 	}),
 });
 
-var message = document.getElementById('message');
-message.innerHTML = "waiting...";
-
+var msg = document.getElementById('message');
+msg.innerHTML = "Waiting...";
+var line = null;
 $(map.getViewport()).on('mousemove', function(evt) {
-	// var pixel = map.getEventPixel(evt.originalEvent);
-	// var hit = map.forEachFeatureAtPixel(pixel, function(feature, layer) {
-	// 	return true;
-	// });
-	// if (hit) {
-	// 	map.getTarget().style.cursor = 'pointer';
-	// } else {
-	// 	map.getTarget().style.cursor = '';
-	// }
-
-	var l = map.getLayers().item(1);
-	var s = l.getSource();
-
-	var mouse_coord = map.getEventCoordinate(evt.originalEvent);
-
-	var feature = s.getClosestFeatureToCoordinate(mouse_coord);
-	var geom = feature.getGeometry();
-	var feature_coord = geom.getClosestPoint(mouse_coord);
-
-	var line = new ol.geom.LineString([mouse_coord, feature_coord]);
-
-	var dummy = 42;
-
-	if (line) {
-		// message.innerHTML = closest.get('description');
-		message.innerHTML = line.getLength();
+	var pixel = map.getEventPixel(evt.originalEvent);
+	var hit = map.forEachFeatureAtPixel(pixel, function(feature, layer) {
+		return true;
+	});
+	if (hit) {
+		map.getTarget().style.cursor = 'pointer';
 	} else {
-		alert ("no closest");
+		map.getTarget().style.cursor = '';
+	}
+
+	var coordinate = map.getEventCoordinate(evt.originalEvent);
+	var closestFeature = vectorSource.getClosestFeatureToCoordinate(coordinate);
+	if (closestFeature === null) {
+		line = null;
+	} else {
+		var geometry = closestFeature.getGeometry();
+		var closestPoint = geometry.getClosestPoint(coordinate);
+		coordinate.push(0);
+		if (line === null) {
+			line = new ol.geom.LineString([coordinate, closestPoint]);
+		} else {
+			line.setCoordinates([coordinate, closestPoint]);
+		}
+		var len = line.getLength();
+		if (len > 100000) {
+			line = null;
+		} else {
+			msg.innerHTML = closestFeature.get('description') + '<br>' + len;
+		}
+	}
+	map.render();
+});
+
+var strokeStyle = new ol.style.Stroke({
+	color: 'rgba(255,255,0,0.9)',
+	width: 3
+});
+map.on('postcompose', function(evt) {
+	var vectorContext = evt.vectorContext;
+	if (line !== null) {
+		vectorContext.setFillStrokeStyle(null, strokeStyle);
+		vectorContext.drawLineStringGeometry(line);
 	}
 });
 
