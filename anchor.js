805eba1100e8e8445c8dabb16022e9a2506cc562
Richard Russon (flatcap) <richard.russon@gmail.com>
2015-01-28 21:42:54 +0000

    icon styling

--- anchor.js
+++ anchor.js
@@ -1,8 +1,20 @@
-var iconFeature = new ol.Feature({
-	geometry: new ol.geom.Point(ol.proj.transform([-1.985778,53.329759], 'EPSG:4326', 'EPSG:3857'))
+var pt1 = [-2.469176,52.788348];
+var pt2 = [-1.985778,53.329759];
+var pt3 = [-1.392516,52.801634];
+
+var f1 = new ol.Feature({
+	'geometry': new ol.geom.LineString([
+		ol.proj.transform(pt1, 'EPSG:4326', 'EPSG:3857'),
+		ol.proj.transform(pt2, 'EPSG:4326', 'EPSG:3857'),
+		ol.proj.transform(pt3, 'EPSG:4326', 'EPSG:3857'),
+	])
 });
 
-var iconStyle = new ol.style.Style({
+var f2 = new ol.Feature({ geometry: new ol.geom.Point(ol.proj.transform(pt1, 'EPSG:4326', 'EPSG:3857')) });
+var f3 = new ol.Feature({ geometry: new ol.geom.Point(ol.proj.transform(pt2, 'EPSG:4326', 'EPSG:3857')) });
+var f4 = new ol.Feature({ geometry: new ol.geom.Point(ol.proj.transform(pt3, 'EPSG:4326', 'EPSG:3857')) });
+
+var i2 = new ol.style.Style({
 	image: new ol.style.Icon({
 		anchor: [0.5, 1.0],
 		anchorXUnits: 'fraction',
@@ -12,7 +24,29 @@ var iconStyle = new ol.style.Style({
 	})
 });
 
-iconFeature.setStyle(iconStyle);
+var i3 = new ol.style.Style({
+	image: new ol.style.Icon({
+		anchor: [0.5, 1.0],
+		anchorXUnits: 'fraction',
+		anchorYUnits: 'fraction',
+		opacity: 1.0,
+		src: 'gfx/r_yellow.png'
+	})
+});
+
+var i4 = new ol.style.Style({
+	image: new ol.style.Icon({
+		anchor: [0.5, 1.0],
+		anchorXUnits: 'fraction',
+		anchorYUnits: 'fraction',
+		opacity: 1.0,
+		src: 'gfx/r_red.png'
+	})
+});
+
+f2.setStyle(i2);
+f3.setStyle(i3);
+f4.setStyle(i4);
 
 var map = new ol.Map({
 	target: 'map',
@@ -25,14 +59,7 @@ var map = new ol.Map({
 		}),
 		new ol.layer.Vector({
 			source: new ol.source.Vector({
-				features: [ new ol.Feature({
-					'geometry': new ol.geom.LineString([
-						ol.proj.transform([-2.469176,52.788348], 'EPSG:4326', 'EPSG:3857'),
-						ol.proj.transform([-1.985778,53.329759], 'EPSG:4326', 'EPSG:3857'),
-						ol.proj.transform([-1.392516,52.801634], 'EPSG:4326', 'EPSG:3857'),
-					])
-				}),
-				iconFeature ],
+				features: [ f1, f2, f3, f4 ],
 			}),
 			style: new ol.style.Style({
 				stroke: new ol.style.Stroke({
